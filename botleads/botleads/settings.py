d4d83ab725637633c439209a42853b0ff307fc3d
Comrade <r1E3AqGc>
2020-07-01 23:17:32 +0300

    final

--- botleads/botleads/settings.py
+++ botleads/botleads/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'botleads.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'botleads',
-        'USER': 'botleads',
-        'PASSWORD': 'botleads',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -129,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
